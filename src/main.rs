00fbc10e05b84aafb737eb13352ebee707c494c9
Radoslav Georgiev <rgeorgiev583@gmail.com>
2021-04-20 18:25:18 +0300

    remove the handling of SIGUSR1: it does not work at all

--- src/main.rs
+++ src/main.rs
@@ -6,30 +6,13 @@ mod log;
 mod varlink;
 mod watcher;
 
-#[cfg(not(windows))]
-use signal_hook::{consts::signal::SIGUSR1, iterator::Signals};
 use std::env::args;
 use std::path::Path;
-#[cfg(not(windows))]
-use std::thread::spawn;
 
 use crate::{error::Result, log::log_error, varlink::VarlinkServer, watcher::Watcher};
 
 fn main() -> Result<()> {
     let watcher = Watcher::new()?;
-
-    if !cfg!(windows) {
-        let cache = watcher.cache.clone();
-        spawn(move || -> Result<()> {
-            let mut signals = Signals::new(&[SIGUSR1])?;
-            for signal in signals.forever() {
-                if let SIGUSR1 = signal {
-                    print!("{}", cache);
-                }
-            }
-            Ok(())
-        });
-    }
 
     for path in args().skip(1) {
         let result = watcher.watch(Path::new(&path));
